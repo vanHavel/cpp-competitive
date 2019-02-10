be58b8d59fd74eccdce1a3cf8da1cf406d99515c
Lukas <dev.lukas.huwald@gmail.com>
2019-02-10 17:33:41 +0100

    fix typo

--- library/Random.h
+++ library/Random.h
@@ -5,13 +5,13 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
-
-
+// gets raw safe random generator
 std::mt19937 getMt() {
     std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
     return rng;
 }
 
+// safely generates random ints in range [min, max]
 auto rangeMt(int min, int max) {
     auto rng = std::bind(
             std::uniform_int_distribution<int>(min, max),
@@ -20,9 +20,12 @@ auto rangeMt(int min, int max) {
     return rng;
 }
 
+// seed rand with time
 void seedRand() {
-    std::srand(std::time(nullptr))
+    std::srand(std::time(nullptr));
 }
+
+// two invocations of rand for decent randomness. Only use as quick workaround.
 int genRand() {
     return std::rand() * 32768 + std::rand();
 }
