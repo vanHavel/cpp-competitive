646bce5a66790e3dd97ff21c93ca3a04e9e90228
Lukas <dev.lukas.huwald@gmail.com>
2019-02-24 13:26:32 +0100

    fixed bugs in range updates

--- library/RangeSumSegmentTree.h
+++ library/RangeSumSegmentTree.h
@@ -6,31 +6,32 @@
 
 class RangeSumSegmentTree {
 private:
-    vi st, A, lazy;
+    vll st, A, lazy;
     int n;
     int left(int parent) { return parent << 1; }
     int right(int parent) { return (parent << 1) + 1; }
 
     void build(int parent, int leftBound, int rightBound) {
         if (leftBound == rightBound) {
-            st[parent] = leftBound;
+            st[parent] = A[leftBound];
         }
         else {
-            build(left(parent), leftBound, (leftBound + rightBound) / 2);
-            build(right(parent), (leftBound + rightBound) / 2 + 1, rightBound);
-            int r1 = st[left(parent)], r2 = st[right(parent)];
-            st[parent] = r1 + r2;
+            int middle = (leftBound + rightBound) / 2;
+            build(left(parent), leftBound, middle);
+            build(right(parent), middle + 1, rightBound);
+            st[parent] = st[left(parent)] + st[right(parent)];
         }
     }
 
-    int rsq(int parent, int leftBound, int rightBound, int i, int j) {
+    ll rsq(int parent, int leftBound, int rightBound, int i, int j) {
         lazyUpdate(parent, leftBound, rightBound);
 
-        if (i > rightBound || j < leftBound) { return 0; }
+        if (i > rightBound || j < leftBound || leftBound > rightBound) { return 0; }
         if (leftBound >= i && rightBound <= j) { return st[parent]; }
 
-        int r1 = rsq(left(parent), leftBound, (leftBound + rightBound) / 2, i, j);
-        int r2 = rsq(right(parent), (leftBound + rightBound) / 2 + 1, rightBound, i, j);
+        int middle = (leftBound + rightBound) / 2;
+        ll r1 = rsq(left(parent), leftBound, middle, i, j);
+        ll r2 = rsq(right(parent), middle + 1, rightBound, i, j);
         return r1 + r2;
     }
 
@@ -45,13 +46,13 @@ private:
         }
     }
 
-    void updateRange(int parent, int leftBound, int rightBound, int updateLeft, int updateRight, int diff) {
+    void updateRange(int parent, int leftBound, int rightBound, int updateLeft, int updateRight, ll diff) {
         lazyUpdate(parent, leftBound, rightBound);
         // stop recursing
         if (leftBound > rightBound || leftBound > updateRight || rightBound < updateLeft) {
             return;
         }
-        // update in range
+        // range in update
         if (leftBound >= updateLeft && rightBound <= updateRight) {
             st[parent] += (rightBound - leftBound + 1) * diff;
             if (leftBound < rightBound) {
@@ -64,11 +65,12 @@ private:
             int middle = (leftBound + rightBound) / 2;
             updateRange(left(parent), leftBound, middle, updateLeft, updateRight, diff);
             updateRange(right(parent), middle + 1, rightBound, updateLeft, updateRight, diff);
+            st[parent] = st[left(parent)] + st[right(parent)];
         }
     }
 
 public:
-    RangeSumSegmentTree(vi &_A) {
+    RangeSumSegmentTree(vll const &_A) {
         A = _A;
         n = (int) A.size();
         st.assign(4*n, 0);
@@ -76,15 +78,15 @@ public:
         build(1, 0, n-1);
     }
 
-    int rangeSumQuery(int i, int j) {
+    ll rangeSumQuery(int i, int j) {
         return rsq(1, 0, n-1, i, j);
     }
 
-    void update(int i, int diff) {
+    void update(int i, ll diff) {
         updateRange(1, 0, n-1, i, i, diff);
     }
 
-    void updateRange(int i, int j, int diff) {
+    void updateRange(int i, int j, ll diff) {
         updateRange(1, 0, n-1, i, j, diff);
     }
 
